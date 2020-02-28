3f566b3f065a76b77951e472d44d01dd4719fd03
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 00:40:47 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumerosPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intInicial = 1;
+            int intFinal = 100;
+
+            for (int x = intInicial; x <= intFinal; x++)
+            {
+                //Se quiser imprimir os pares
+                if (x % 2 == 0)  //ou if (x % 2 == 1) //Se quiser imprimir os impares
+                {
+                    Console.WriteLine(x);
+
+                    
+                }
+                //Console.ReadKey();
+            }
+        }
+    }
+}
